126377e0ab94229f5c452f1f91e9adefbb6bea87
Rodinamomen <Rodinamobark3@gmail.com>
2026-04-20 14:01:43 +0200

    wip: Add EvolveFitSpacing with standard spacing tokens

---
+++ app/src/main/java/com/app/evolvefit/ui/theme/space/EvolveFitSpace.kt
@@ -0,0 +1,12 @@
+package com.app.evolvefit.ui.theme.space
+
+import androidx.compose.ui.unit.dp
+
+object EvolveFitSpacing {
+    val none = 0.dp
+    val xSmall = 4.dp
+    val small = 8.dp
+    val medium = 16.dp
+    val large = 24.dp
+    val xLarge = 32.dp
+}
