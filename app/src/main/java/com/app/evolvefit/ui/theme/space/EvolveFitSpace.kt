c6858f7f799bd6ca8c5c6b7888c9f3354fed54f0
Rodinamomen <Rodinamobark3@gmail.com>
2026-04-20 14:07:42 +0200

    wip:Rename EvolveFitSpacing to EvolveFitSpace and update field names for clarity

--- app/src/main/java/com/app/evolvefit/ui/theme/space/EvolveFitSpace.kt
+++ app/src/main/java/com/app/evolvefit/ui/theme/space/EvolveFitSpace.kt
@@ -2,11 +2,11 @@ package com.app.evolvefit.ui.theme.space
 
 import androidx.compose.ui.unit.dp
 
-object EvolveFitSpacing {
+object EvolveFitSpace {
     val none = 0.dp
-    val xSmall = 4.dp
+    val extraSmall = 4.dp
     val small = 8.dp
     val medium = 16.dp
     val large = 24.dp
-    val xLarge = 32.dp
+    val extraLarge = 32.dp
 }
